69cda2ca7e6bc3c2d1eb54ff3bef81c425c83d3b
vishal1145 <vishal1145@gmail.com>
2024-12-07 17:00:06 +0530

    change port

--- index.js
+++ index.js
@@ -44,4 +44,4 @@ app.get('/getReviews', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3005, () => console.log("Server running on port 3005"));
